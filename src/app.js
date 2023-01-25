5809f4819dc24db7698e682acade3a1b55b3c8fc
samuelsankys <ssankys3@gmail.com>
2023-01-24 23:50:14 -0300

    feat: create server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`App financeiro listening on port ${PORT}`);
+});
