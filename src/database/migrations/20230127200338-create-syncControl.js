a8f66dbf6cb723d82cfdb0e28dcff6ca2db1ccbc
samuelsankys <ssankys3@gmail.com>
2023-01-28 23:51:22 -0300

    chore: add fields table sync_control

--- src/database/migrations/20230127200338-create-syncControl.js
+++ src/database/migrations/20230127200338-create-syncControl.js
@@ -4,8 +4,14 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('sync_control', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       file: Sequelize.STRING,
       last_sync_product: Sequelize.STRING,
+      imported_t: Sequelize.DATE,
     })
   },
 
